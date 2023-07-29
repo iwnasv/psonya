7ee19bb09e98e4d3afa6caf1beaaf51062c66453
iwnasv <34813745+iwnasv@users.noreply.github.com>
2023-07-29 22:48:33 +0300

    add api key and complete json dump options

--- psonya_request.py
+++ psonya_request.py
@@ -1,9 +1,12 @@
+import hashlib
 from flask import Flask, request, jsonify
 import sys
 import json
 from fetch_product import ProductFetcher
 
 app = Flask(__name__)
+
+API_KEY_HASH = 'ab8406e9fe6dbd38611d816c4c18ee20c616023c13036e9d85eb2946be899760'
 
 class Product:
     def __init__(self, code, name, price, store, pic):
@@ -15,12 +18,21 @@ class Product:
 
 @app.route('/products', methods=['GET'])
 def get_product():
-    # Get the product ID from the query parameters
+    # Get the product ID and API key from the query parameters
     product_id = request.args.get('id', type=int)
+    api_key = request.args.get('key')
 
     # Check if the ID is provided and valid
     if product_id is None:
         return jsonify({"error": "Product ID not provided."}), 400
+
+    # Check if the API key is provided and valid
+    if api_key is None:
+        return jsonify({"error": "API key not provided."}), 400
+
+    # Compare the hash
+    if hashlib.sha256(api_key.encode()).hexdigest() != API_KEY_HASH:
+        return jsonify({"error": "Invalid API key."}), 403
 
     if '-x' in sys.argv:
         fetcher = ProductFetcher()
@@ -33,6 +45,12 @@ def get_product():
         return response
 
     else:
+        if product_id == 0:
+            # Read and return the whole JSON file
+            with open('products.json', 'r') as json_file:
+                data = json.load(json_file)
+            return jsonify(data)
+
         with open('products.json', 'r') as json_file:
             data = json.load(json_file)
 
